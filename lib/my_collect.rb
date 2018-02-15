1dbff0bc84bdfea5c9b3804ed8e5a7a0746182f0
Veronica Bustabad <vbustabad@gmail.com>
2018-02-15 19:23:06 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,12 +2,15 @@ def my_collect(collection)
   if block_given?
   i = 0
   new_collection = []
-    
+
   while i < collection.length
     new_collection = yield(collection[i])
     i += 1
-  end 
-  
-  new_collection
-end 
+  end
 
+  new_collection
+
+  else
+    return 0
+  end
+end
